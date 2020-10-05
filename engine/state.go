cea-hpc/fleet

commit 9bcb7d7db13413f82d4046e19524c4a238047f5a

engine/state: Fix potential data races

engine/state.go
@@ -15,6 +15,8 @@
 package engine
 
 import (
+	"sync"
+
 	"github.com/cea-hpc/fleet/agent"
 	"github.com/cea-hpc/fleet/job"
 	"github.com/cea-hpc/fleet/machine"
@@ -24,6 +26,7 @@ type clusterState struct {
 	jobs     map[string]*job.Job
 	gUnits   map[string]*job.Unit
 	machines map[string]*machine.MachineState
+	mu       *sync.RWMutex
 }
 
 func newClusterState(units []job.Unit, sUnits []job.ScheduledUnit, machines []machine.MachineState) *clusterState {
@@ -65,6 +68,7 @@ func newClusterState(units []job.Unit, sUnits []job.ScheduledUnit, machines []ma
 		jobs:     jMap,
 		gUnits:   guMap,
 		machines: mMap,
+		mu:       new(sync.RWMutex),
 	}
 }
 
@@ -74,6 +78,9 @@ func (cs *clusterState) agents() map[string]*agent.AgentState {
 		ms := ms
 		agents[ms.ID] = agent.NewAgentState(ms)
 	}
+
+	cs.mu.Lock()
+	defer cs.mu.Unlock()
 
 	for _, j := range cs.jobs {
 		j := j
@@ -109,6 +116,9 @@ func (cs *clusterState) agents() map[string]*agent.AgentState {
 }
 
 func (cs *clusterState) schedule(jobName, targetMachineID string) {
+	cs.mu.Lock()
+	defer cs.mu.Unlock()
+
 	j := cs.jobs[jobName]
 	if j == nil {
 		return
@@ -117,6 +127,9 @@ func (cs *clusterState) schedule(jobName, targetMachineID string) {
 }
 
 func (cs *clusterState) unschedule(jobName string) {
+	cs.mu.Lock()
+	defer cs.mu.Unlock()
+
 	j := cs.jobs[jobName]
 	if j == nil {
 		return

engine/state_test.go
@@ -17,6 +17,7 @@ package engine
 import (
 	"fmt"
 	"reflect"
+	"sync"
 	"testing"
 
 	"github.com/cea-hpc/fleet/agent"
@@ -44,6 +45,7 @@ func TestClusterStateAgents(t *testing.T) {
 			clust: &clusterState{
 				jobs:     map[string]*job.Job{},
 				machines: map[string]*machine.MachineState{},
+				mu:       new(sync.RWMutex),
 			},
 			agents: map[string]*agent.AgentState{},
 		},
@@ -58,6 +60,7 @@ func TestClusterStateAgents(t *testing.T) {
 					},
 				},
 				machines: map[string]*machine.MachineState{},
+				mu:       new(sync.RWMutex),
 			},
 			agents: map[string]*agent.AgentState{},
 		},
@@ -69,6 +72,7 @@ func TestClusterStateAgents(t *testing.T) {
 				machines: map[string]*machine.MachineState{
 					"XXX": &machine.MachineState{ID: "XXX"},
 				},
+				mu: new(sync.RWMutex),
 			},
 			agents: map[string]*agent.AgentState{
 				"XXX": &agent.AgentState{
@@ -101,6 +105,7 @@ func TestClusterStateAgents(t *testing.T) {
 				machines: map[string]*machine.MachineState{
 					"XXX": &machine.MachineState{ID: "XXX"},
 				},
+				mu: new(sync.RWMutex),
 			},
 			agents: map[string]*agent.AgentState{
 				"XXX": &agent.AgentState{
@@ -139,6 +144,7 @@ func TestClusterStateAgents(t *testing.T) {
 					"YYY": &machine.MachineState{ID: "YYY"},
 					"ZZZ": &machine.MachineState{ID: "ZZZ"},
 				},
+				mu: new(sync.RWMutex),
 			},
 			agents: map[string]*agent.AgentState{
 				"XXX": &agent.AgentState{
@@ -219,6 +225,7 @@ func TestClusterStateAgents(t *testing.T) {
 						},
 					},
 				},
+				mu: new(sync.RWMutex),
 			},
 			agents: map[string]*agent.AgentState{
 				"XXX": &agent.AgentState{
@@ -294,6 +301,7 @@ func TestClusterStateAgents(t *testing.T) {
 					"YYY": &machine.MachineState{ID: "YYY"},
 					"ZZZ": &machine.MachineState{ID: "ZZZ"},
 				},
+				mu: new(sync.RWMutex),
 			},
 			agents: map[string]*agent.AgentState{
 				"XXX": &agent.AgentState{

commit 468b1f654206b90ce572991bb7febe46f8f0e6ca

Fix deadlock introduced by 9bcb7d7d

engine/state.go
@@ -79,8 +79,8 @@ func (cs *clusterState) agents() map[string]*agent.AgentState {
 		agents[ms.ID] = agent.NewAgentState(ms)
 	}
 
-	cs.mu.Lock()
-	defer cs.mu.Unlock()
+	cs.mu.RLock()
+	defer cs.mu.RUnlock()
 
 	for _, j := range cs.jobs {
 		j := j

registry/rpc/inmemory.go
@@ -89,8 +89,6 @@ func (r *inmemoryRegistry) Schedule() (units []pb.ScheduledUnit, err error) {
 	}
 	r.mu.RLock()
 	defer r.mu.RUnlock()
-	r.unitStatesMu.Lock()
-	defer r.unitStatesMu.Unlock()
 
 	units = make([]pb.ScheduledUnit, 0, len(r.scheduledUnits))
 	for _, schedUnit := range r.scheduledUnits {
@@ -365,8 +363,8 @@ func (r *inmemoryRegistry) statesByMUSKey() map[registry.MUSKey]*pb.UnitState {
 }
 
 func (r *inmemoryRegistry) isUnitLoaded(unitName, machineID string) bool {
-	r.unitStatesMu.Lock()
-	defer r.unitStatesMu.Unlock()
+	r.unitStatesMu.RLock()
+	defer r.unitStatesMu.RUnlock()
 	if _, exists := r.unitStates[unitName]; exists {
 		if _, exists := r.unitStates[unitName][machineID]; exists {
 			return true
@@ -376,8 +374,8 @@ func (r *inmemoryRegistry) isUnitLoaded(unitName, machineID string) bool {
 }
 
 func (r *inmemoryRegistry) isUnitLaunched(unitName, machineID string) bool {
-	r.heartbeatsMu.Lock()
-	defer r.heartbeatsMu.Unlock()
+	r.heartbeatsMu.RLock()
+	defer r.heartbeatsMu.RUnlock()
 	if _, exists := r.unitHeartbeats[unitName]; exists {
 		if _, exists := r.unitHeartbeats[unitName][machineID]; exists {
 			return true